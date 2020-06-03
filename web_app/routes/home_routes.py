438a5ebc6e2a58081700502057d37127b5820b86
zachary-hamilton <57552986+zachary-hamilton@users.noreply.github.com>
2020-06-02 22:49:51 -0400

    initial webapp build

---
+++ web_app/routes/home_routes.py
@@ -0,0 +1,16 @@
+# web_app/routes/home_routes.py
+
+from flask import Blueprint
+
+home_routes = Blueprint("home_routes", __name__)
+
+@home_routes.route("/")
+def index():
+    print('VISITING THE HOME PAGE')
+    x = 2 + 2
+    return f"Hello World! {x}"
+
+@home_routes.route("/about")
+def about():
+    print('VISITING THE ABOUT PAGE')
+    return "About me"
