f6cd2edda2b416e151b1fc7515beabfc135c6a1c
zachary-hamilton <57552986+zachary-hamilton@users.noreply.github.com>
2020-06-04 22:28:08 -0400

    interfacing with basilica service

---
+++ web_app/services/basilica_service.py
@@ -0,0 +1,18 @@
+# web_app/services/basilica_service.py
+
+import basilica
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+BASILICA_API_KEY = os.getenv('BASILICA_API_KEY')
+
+connection = basilica.Connection(BASILICA_API_KEY)
+
+
+if __name__ == '__main__':
+    sentences = ["Hello world!", "How are you?"]
+    embeddings = connection.embed_sentences(sentences)
+    print(list(embeddings)) # [[0.8556405305862427, ...], ...]
