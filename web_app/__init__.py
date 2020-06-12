acbe88b548ea6573810bc59eaf3628aba0e1efbf
zachary-hamilton <57552986+zachary-hamilton@users.noreply.github.com>
2020-06-11 22:31:27 -0400

    fixing error

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -11,7 +11,7 @@ from web_app.routes.twitter_routes import twitter_routes
 from web_app.routes.stats_routes import stats_routes
 
 
-DATABASE_URI = "sqlite:///twitoff_development_pt5.db" # using relative filepath
+DATABASE_URI = DATABASE_URL # using relative filepath
 #DATABASE_URI = "sqlite:////Users/Username/Desktop/your-repo-name/web_app_99.db" # using absolute filepath on Mac (recommended)
 #DATABASE_URI = "sqlite:///C:\\Users\\Username\\Desktop\\your-repo-name\\web_app_99.db" # using absolute filepath on Windows (recommended) h/t: https://stackoverflow.com/a/19262231/670433
 
